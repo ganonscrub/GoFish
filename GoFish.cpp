c2d8cef033b5b43aab1f0a7f02ca6b998abea9d8
decampbell <d.dan.campbell@comcast.net>
2015-03-09 17:41:17 -0400

    Update GoFish.cpp

--- GoFish.cpp
+++ GoFish.cpp
@@ -4,18 +4,33 @@
 
 #include "Game.h"
 
-int main( int argc, char* argv )
+int main(int argc, char* argv)
 {
-	srand( (unsigned)time( NULL ) );
+	srand((unsigned)time(NULL));
+
+	system("mode 120, 42");
 
 	int numOfPlayers = 0;
+	int  T = 0;
 
-	std::cout << "Enter the number of players( 2-5 ): ";
-	std::cin >> numOfPlayers;
+	//Would crash if a number larger then 10 was entered or a letter so I reworked it a bit.
+	while (T != 1){
+		std::cout << "Enter the number of players( 2-5 ): ";
+		std::cin >> numOfPlayers;
+		if ((2 <= numOfPlayers) && (numOfPlayers <= 5)){
+			T = 1;
+		}
+		//elsif(numOfPlayers >)
+		else{
+			std::cout << "Invalid number of players\n\n\n";
+			std::cin.clear();
+			std::cin.ignore();
+		}
+	}
 
 	system("cls");
 
-	Game game( numOfPlayers );
+	Game game(numOfPlayers);
 
 	game.run();
 
